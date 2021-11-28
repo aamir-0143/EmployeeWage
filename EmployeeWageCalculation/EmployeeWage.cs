95af73b5047b1681f4d2315637380b214bbf91e1
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-11-28 22:16:26 +0530

    [Aamir] Add ; Added TotalWorkingDays

--- EmployeeWageCalculation/EmployeeWage.cs
+++ EmployeeWageCalculation/EmployeeWage.cs
@@ -8,14 +8,18 @@ namespace EmployeeWageCalculation
 {
     public class EmployeeWage
     {
+        //Constants
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAY = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
         //Variables
         int empHrs = 0;
-        int empWage = 0;
         int totalEmpWage = 0;
+        int empWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
         public void Attendance()
         {
             Random rand = new Random();
@@ -93,7 +97,7 @@ namespace EmployeeWageCalculation
         public void MonthWages()
         {
             Random random = new Random();
-            for (int day = 0; day <NUM_OF_WORKING_DAY; day++)
+            for (int day = 0; day <NUM_OF_WORKING_DAYS; day++)
             {
                 int empcheck = random.Next(0, 3);
                 switch (empcheck)
@@ -113,6 +117,31 @@ namespace EmployeeWageCalculation
                 Console.WriteLine("Emp Wage : " + empWage);
             }
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+        public void TotalWorkinDays()   
+        {
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("total Emp Hrs : " + totalEmpWage);
         }
 
     }
--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -5,3 +5,4 @@ employee.DailyWage();
 employee.PartTime();
 employee.CaseStatement();
 employee.MonthWages();
+employee.TotalWorkinDays();
